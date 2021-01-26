0a15ee649ad69ba32f2042cd07668589b76bf174
Basil <Vaseokk@gmail.com>
2021-01-26 13:09:10 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import  Terminal from './Terminal';
 
 
 function App() {
-  const initialCount = () => Number(JSON.parse(sessionStorage.getItem('id')|| 1));
+  const initialCount = JSON.parse(sessionStorage.getItem('id')|| 1);
   const [countTerminals, setCountTerminals] = useState(initialCount);
   const terminals = [];
 
@@ -23,7 +23,7 @@ function App() {
   for (let i = 0; i < countTerminals; i++) {
     terminals.push(
       <Terminal
-        key={i}
+        i={i}
         addTerminalHandler={addTerminalHandler}
         killTerminalHandler={killTerminalHandler}
         countTerminals={countTerminals}
@@ -40,95 +40,14 @@ function App() {
     >
       {terminals.map((item, index) => {
         return (
-          <div className="item" key={index} >
+          <div className="item" key={index}>
             {item}
           </div>
         );
       })}
     </div>
 
-    // <div className="App">
-    //     {appState.terminal.map((item)=> {
-    //     return <div className="item" key={item.id} >
-    //       {item.terminal}
-    //       {...}
-    //        this is {item.id}
-
-    //       </div>;})}
-    // </div>
   );
 }
 
 export default App;
-
-
-
-
-
-// import React, { useState, useEffect } from "react";
-
-// import "./App.scss";
-// import Terminal from "./Terminal";
-
-// function App() {
-//   const initialCount = () =>
-//     Number(sessionStorage.getItem("terminalCounter") || 0);
-//   const [terminal, setTerminal] = useState(initialCount);
-//   const date = new Date();
-//   const terminalCounter = 1;
-//   const [appState, setAppState] = useState({
-//     terminal: [
-//       {
-//         id: terminalCounter,
-//         createdAt: date,
-//       },
-//     ],
-//     terminalCounter: terminalCounter,
-//   });
-
-//   let id = terminal;
-
-//   useEffect(() => {
-//     sessionStorage.setItem("terminal", terminalCounter);
-//     sessionStorage.setItem("id", terminalCounter);
-//   }, [terminal, id]);
-
-//   // salve in browser history
-//   // how to implement terminal sesion
-//   // console.log(createRef);
-//   function AddTerminal() {
-//     return (
-//       <Terminal
-//         setTerminal={setTerminal}
-//         terminal={terminal}
-//         id={terminalCounter}
-//       />
-//     );
-//   }
-
-//   return (
-//     <div
-//       className={`App ${id >= 1 && "splited"} ${id >= 2 && "splited-1"} ${
-//         id >= 4 && "splited-2"
-//       }`}
-//     >
-//       {Array(terminalCounter)
-//         .fill(<AddTerminal setTerminal={setTerminal} terminal={terminal} />)
-//         .map((element, terminal) => {
-//           return (
-//             <div
-//               className={`item ${id === 1 && "item-1"} ${
-//                 id === 2 && "item-2"
-//               } ${id === 2 && "item-2 addon"}`}
-//               key={terminal}
-//             >
-//               {element}
-//               {`${`${id} created at ${new Date()}`}`}
-//             </div>
-//           );
-//         })}
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -7,20 +7,20 @@ import NavItem from './NavItem';
 import DropdownMenu from './DropdownMenu';
 import './Terminal.scss';
 
-function Terminal({ countTerminals, addTerminalHandler, killTerminalHandler,terminals }) {
+function Terminal({ countTerminals, addTerminalHandler, killTerminalHandler, index }) {
   const [value, setValue] = useState("");
   const [ws] = useState(new WebSocket("ws://127.0.0.1:8080"));
+  let getData = JSON.parse(sessionStorage.getItem(countTerminals));
+  console.log(getData);
+  for (let i = 0; i < getData; i++) {
+    console.log(getData);
+  }
+  console.log(sessionStorage);
   const [inputs, setInputs] = useState([]);
-  const [commandsArr, setCommandsArr] = useState([]);
+  const [commandsArr, setCommandsArr] = useState(getData? getData.commands : []);
   const [count, setCount] = useState(0);
   const inputFocus = useRef(null);
   const [showColorPicker, setShowColorPicker] = useState(false);
-  let getData = JSON.parse(sessionStorage.getItem(countTerminals))
-  console.log(getData)
-  for (let i = 0; i < getData; i++){
-    // console.log(getData);
-  }
-  console.log(sessionStorage)
   const [color, setColor] = useState(getData? getData.bgColor : '#00000' );
   const [showTextColorPicker, setShowTextColorPicker] = useState(false);
   const [textColor, setTextColor] = useState(getData? getData.txtColor : '#fff');
@@ -44,30 +44,18 @@ function Terminal({ countTerminals, addTerminalHandler, killTerminalHandler,term
         }
       };
     }
-
+  
     const session = {
       id: countTerminals,
       txtColor: textColor,
       bgColor: color,
       textSize: changeTextSize,
-    }
+      inputs: inputs,
+      commands: commandsArr,
+    };
     sessionStorage.setItem(countTerminals, JSON.stringify(session));
-  
 
-
-    // sessionStorage.setItem("txtColor", textColor);
-    // sessionStorage.setItem("bgColor", color);
-    // sessionStorage.setItem("textSize", changeTextSize);
-
-    //TODO: set in local storage the data that changes
-    // sessionStorage.setItem("txtColor", textColor);
-    // sessionStorage.setItem("bgColor", color);
-    // sessionStorage.setItem("textSize", changeTextSize);
-    // function onFocusHandle() {
-    //   const input = document.querySelector('input');
-    //   input.focus();
-    // }
-  }, [ws, commandsArr, textColor, color, changeTextSize, countTerminals]);
+  }, [ws, commandsArr, textColor, color, changeTextSize, countTerminals,inputs]);
   // the arrow up and down function
   function onArrowsHandle(e) {
     if (commandsArr.length > 0) {
@@ -149,7 +137,7 @@ function Terminal({ countTerminals, addTerminalHandler, killTerminalHandler,term
 
   return (
     <div
-      className="terminal"
+      className={`terminal`}
       style={{
         backgroundColor: color,
         color: textColor,
@@ -163,9 +151,8 @@ function Terminal({ countTerminals, addTerminalHandler, killTerminalHandler,term
               <div className="dropdown-item-block">
                 <div
                   className="dropdown-item"
-                  onClick={()=>
-                   countTerminals > 1 ?  killTerminalHandler() : null
-                    
+                  onClick={() =>
+                    countTerminals > 1 ? killTerminalHandler() : null
                   }
                 >
                   Kill Terminal
@@ -213,13 +200,13 @@ function Terminal({ countTerminals, addTerminalHandler, killTerminalHandler,term
         {showColorPicker && (
           <ChromePicker
             color={color}
-            onChange={updatedColor => setColor(updatedColor.hex)}
+            onChange={(updatedColor) => setColor(updatedColor.hex)}
             onChangeComplete={onChangeColorClose}
           />
         )}
         {showTextColorPicker && (
           <ChromePicker
-            styles={{position: 'absolute'}}
+            styles={{ position: "absolute" }}
             color={textColor}
             onChange={(updatedColor) => setTextColor(updatedColor.hex)}
             onChangeComplete={onChangeTextColorClose}
